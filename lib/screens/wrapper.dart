e033d0a7c718ddc21adbf2293bb4aad7ece68c0d
Shaddy <shadybett540@gmail.com>
2025-03-05 07:27:50 +0300

    made files for home and authentications

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
@@ -5,6 +6,6 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Home();
   }
 }
