98f6940134e93f3360ba3e8d21ad6e06496ac434
Shaddy <shadybett540@gmail.com>
2025-03-12 10:41:11 +0300

    setup folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,70 @@
-import 'package:brew_crew/screens/authenticate/authenticate.dart';
-import 'package:brew_crew/screens/authenticate/practice.dart';
-import 'package:brew_crew/screens/home/home.dart';
-import 'package:brew_crew/screens/wrapper.dart';
-import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:grocery_delivery_app/firebase_options.dart';
+import 'package:grocery_delivery_app/providers/auth_provider.dart';
+import 'package:grocery_delivery_app/providers/cart_provider.dart';
+import 'package:grocery_delivery_app/providers/product_provider.dart';
+import 'package:grocery_delivery_app/router/app_router.dart';
+import 'package:grocery_delivery_app/utils/constants/colors.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  
+  runApp(EasyLocalization(
+    path: 'assets/lang',
+    supportedLocales: const [Locale('en')],
+    fallbackLocale: const Locale('en'),
+    useFallbackTranslations: true,
+    child: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    requestPermissions();
+  }
+
+  Future<void> requestPermissions() async {
+    final notificationStatus = await Permission.notification.status;
+    if (!notificationStatus.isGranted) {
+      await Permission.notification.request();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthProvider()),
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+        ChangeNotifierProvider(create: (context) => ProductProvider()),
+      ],
+      child: MaterialApp(
+        initialRoute: AppRouter.initial,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        title: 'Grocery Delivery',
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        theme: ThemeData(
+          primaryColor: primaryColor,
+          fontFamily: 'SpaceGrotesk',
+        ),
+      ),
     );
   }
 }
